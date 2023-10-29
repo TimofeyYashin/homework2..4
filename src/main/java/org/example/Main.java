64091b4e8183d7497cbadbd324811e47b73043cc
г <ashin.timoti2018@gmail.com>
2023-10-29 22:41:07 +0300

    homework2.4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String login = "login1234!";
+        String password = "password1234";
+        String confirmPassword = "password1234";
+
+       try {
+           Validator.validate(login,password,confirmPassword);
+           System.out.println("Валидация прошла успешно");
+       } catch (WrongLoginException wrongLoginException) {
+           wrongLoginException.printStackTrace();
+       }
+       catch (WrongPasswordException wrongPasswordException) {
+          wrongPasswordException.printStackTrace();
+       }
+        System.out.println("Валидация завершилась");
     }
 }
